2ad910cfba2e1f929d4254ad4c09cb92ee7c75bf
natabask <natabask@yandex.ru>
2022-04-28 20:02:20 +0300

    initial commit (simple Tests added)

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,60 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+        // ниже граничного значения
+    void shouldCountWhenUnderBoundary() {
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+        // ниже граничного значения на 1
+    void shouldCountWhenCloseToBoundaryUnder() {
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+        // равно граничному значению
+    void shouldCountWhenBoundary() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+        // ниже граничного значения на 1
+    void shouldCountWhenCloseToBoundaryBelow() {
+
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+        // ниже граничного значения на 1
+    void shouldCountWhenAboveBoundary() {
+
+        int actual = service.remain(1050);
+        int expected = 950;
+
+        assertEquals(actual, expected);
+    }
+}
