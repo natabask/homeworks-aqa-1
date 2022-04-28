f74aa1aeb6ea1c43aac65f32577c9c7bb2bd5ef9
natabask <natabask@yandex.ru>
2022-04-28 20:20:27 +0300

    initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,60 +1,60 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
     @Test
-        // ниже граничного значения
-    void shouldCountWhenUnderBoundary() {
+    // ниже граничного значения
+    public void shouldCountWhenUnderBoundary() {
 
         int actual = service.remain(900);
         int expected = 100;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-        // ниже граничного значения на 1
-    void shouldCountWhenCloseToBoundaryUnder() {
+    // ниже граничного значения на 1
+    public void shouldCountWhenCloseToBoundaryUnder() {
 
         int actual = service.remain(999);
         int expected = 1;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-        // равно граничному значению
-    void shouldCountWhenBoundary() {
+    // равно граничному значению
+    public void shouldCountWhenBoundary() {
 
         int actual = service.remain(1000);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-        // ниже граничного значения на 1
-    void shouldCountWhenCloseToBoundaryBelow() {
+    // выше граничного значения на 1
+    public void shouldCountWhenCloseToBoundaryBelow() {
 
         int actual = service.remain(1001);
         int expected = 999;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-        // ниже граничного значения на 1
-    void shouldCountWhenAboveBoundary() {
+    // выше граничного значения
+    public void shouldCountWhenAboveBoundary() {
 
         int actual = service.remain(1050);
         int expected = 950;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
